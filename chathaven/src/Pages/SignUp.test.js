c822496510a31434b5e3553794d1508b98b4c4a5
KhaledDaoud9 <khaled2510@gmail.com>
2025-03-21 13:31:42 -0400

    Create SignUp.test.js
    Signed-off-by: KhaledDaoud9 <khaled2510@gmail.com>

---
+++ chathaven/src/Pages/SignUp.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import Signup from "./Signup"; 
+
+describe("Signup Page Tests", () => {
+  test("Signup page renders correctly", () => {
+    render(<Signup />);
+    expect(screen.getByPlaceholderText("Enter your name")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Enter your email")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Enter your password")).toBeInTheDocument();
+  });
+});
